65556a2c9eb8a5dd5cdf99f838334e9cea7743e6
Alexandr Shunyaev <newroad1993@gmail.com>
2024-01-03 00:13:00 +0300

    Docker compose

--- docker_compose/simple_project/app/movies/api/v1/views.py
+++ docker_compose/simple_project/app/movies/api/v1/views.py
@@ -1,5 +1,15 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
-def api(request):
-    return HttpResponse("My best API")
+# def api(request):
+#     return HttpResponse("My best API")
+
+from django.http import JsonResponse
+from django.views import View
+
+class MoviesListApi(View):
+    http_method_names = ['get']
+
+    def get(self, request, *args, **kwargs):
+        # Получение и обработка данных
+        return JsonResponse({}) 
